8c459a7ed3b3614d4008a52661859db1937cc930
Fajar Fitrianto <60049222+fajarFWD@users.noreply.github.com>
2020-04-01 07:15:48 +0700

    Update welcome_sql.py

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 from tg_bot.modules.helper_funcs.msg_types import Types
 from tg_bot.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = "Hey {first}, how are you?"
-DEFAULT_GOODBYE = "Nice knowing ya!"
+DEFAULT_WELCOME = "Halo {first},Kuy join di @daftarputar (https://t.me/daftarputar) kalo kamu mau download film gratis tanpa iklan yang menjengelkan."
+DEFAULT_GOODBYE = "Senang mengenalmu... @daftarputar!"
 
 
 class Welcome(BASE):
